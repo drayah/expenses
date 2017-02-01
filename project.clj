c1c97cdbd83027642a97cde433433dd3301b6f95
Giovanni Martina <gio@popado.co>
2017-02-01 20:24:32 -0200

    Initial project setup

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject expenses "0.1.0-SNAPSHOT"
+  :description "Personal finances hobby project"
+  :url "https://github.com/drayah/expenses"
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
---
+++ src/finance/core.clj
@@ -0,0 +1,61 @@
+(ns finance.core 
+  (:require [clojure.java.io :as cio]
+            [clojure.string :as str]))
+
+(defn real-return) 
+  "Calculate the real rate of return given a nominal and inflation rate"
+  [nominal inflation]
+  (let [nominal (/ nominal 100)
+        inflation (/ inflation 100)
+        real-rate (- (/ (+ 1 nominal) (+ 1 inflation)) 1)]
+    (* real-rate 100))
+
+(defn keyword-for-index 
+  "Returns a keyword for index"
+  [index]
+  (case index
+    0  :date
+    1  :amount
+    2  :category
+    3  :subcategory
+    4  :type
+    7  :payee
+    10 :account
+       :unknown))
+
+(defn parse-value
+  "Parse a string value to a correct type"
+  [keyword value]
+  (case keyword
+    :amount (if (empty? value) 0M (bigdec value))
+            value))
+    
+(defn build-map
+  "Given a seq of strings build an expense map"
+  [strings]
+  (loop [current strings
+         index 0
+         map {}]
+    (if (empty? current) 
+      map
+      (let [keyword (keyword-for-index index)
+            value (parse-value keyword (first current))
+            map (if (= keyword :unknown) map (assoc map keyword value))]
+        (recur (rest current) (inc index) map)))))
+
+(defn line->expense
+  "Transform an expense line to a map"
+  [line]
+  (let [elements (str/split line #";")]
+    (build-map elements)))
+
+(defn csv->expenses
+  "Transform a seq of csv strings to a seq of expenses"
+  [csv]
+  (map line->expense csv))
+
+(defn read-expense-csv
+  "Read lines from a csv and return as seq"
+  [file]
+  (with-open [reader (cio/reader file)]
+    (doall (line-seq reader))))
