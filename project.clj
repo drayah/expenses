e5d75dc50f94d98529262d4ec32c487789a6a1ce
Giovanni Martina <gio@popado.co>
2018-08-21 22:46:08 -0300

    Update project dependencies

--- project.clj
+++ project.clj
@@ -1,8 +1,12 @@
 (defproject expenses "0.1.0-SNAPSHOT"
   :description "Personal finances hobby project"
   :url "https://github.com/drayah/expenses"
+  :min-lein-version "2.8.1"
   :license {:name "MIT License"
             :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.9.0"]]
-  :plugins [[lein-expectations "0.0.8"]]
-  :profiles {:dev {:dependencies [[expectations "2.2.0-alpha1"]]}})
+  :plugins [[lein-coverage "1.0.11"]]
+  :profiles {:dev {:dependencies [[midje "1.9.2"]
+                                  [cloverage "1.0.13"]]
+                   :plugins [[lein-midje "3.2.1"]]}
+             :uberjar {:aot :all}})
