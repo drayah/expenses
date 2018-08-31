bb383ac763313c748c1bf77f25c28a3a6fbc73cd
Giovanni Martina <gio@popado.co>
2018-08-30 21:35:08 -0300

    Clean code by removing comments

--- src/co/popado/finance/domain/expenses.clj
+++ src/co/popado/finance/domain/expenses.clj
@@ -2,17 +2,13 @@
   (:require [clojure.java.io :as cio]
             [clojure.string :as str]))
 
-(defn real-return
-  "Calculate the real rate of return given a nominal and inflation rate"
-  [nominal inflation]
+(defn real-return [nominal inflation]
   (let [nominal (/ nominal 100)
         inflation (/ inflation 100)
         real-rate (- (/ (+ 1 nominal) (+ 1 inflation)) 1)]
     (* real-rate 100)))
 
-(defn keyword-for-index
-  "Returns a keyword for index"
-  [index]
+(defn keyword-for-index [index]
   (case index
     0  :date
     1  :amount
@@ -23,16 +19,12 @@
     10 :account
        :unknown))
 
-(defn parse-value
-  "Parse a string value to a correct type"
-  [keyword value]
+(defn parse-value [keyword value]
   (case keyword
     :amount (if (empty? value) 0M (bigdec value))
             value))
 
-(defn build-map
-  "Given a seq of strings build an expense map"
-  [strings]
+(defn build-map [strings]
   (loop [current strings
          index 0
          map {}]
@@ -43,13 +35,9 @@
             map (if (= keyword :unknown) map (assoc map keyword value))]
         (recur (rest current) (inc index) map)))))
 
-(defn line->expense
-  "Transform an expense line to a map"
-  [line]
+(defn line->expense [line]
   (let [elements (str/split line #";")]
     (build-map elements)))
 
-(defn csv->expenses
-  "Transform a seq of csv strings to a seq of expenses"
-  [csv]
+(defn csv->expenses [csv]
   (map line->expense csv))
