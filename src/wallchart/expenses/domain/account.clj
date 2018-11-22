bce5b1dc651205d40b01a5b1ba3302362789fdc3
Giovanni Martina <drayah@gmail.com>
2018-11-22 21:49:09 -0200

    Correctly indent maps

--- src/wallchart/expenses/domain/account.clj
+++ src/wallchart/expenses/domain/account.clj
@@ -5,14 +5,14 @@
   (UUID/randomUUID))
 
 (defn create-account [name]
-  {:id (random-uuid)
-   :name name
+  {:id           (random-uuid)
+   :name         name
    :transactions []})
 
 (defn create-transaction [date amount category]
-  {:id (random-uuid)
-   :date date
-   :amount amount
+  {:id       (random-uuid)
+   :date     date
+   :amount   amount
    :category category})
 
 (defn add-transaction [account transaction]
