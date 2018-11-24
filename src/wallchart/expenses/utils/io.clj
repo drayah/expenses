73dbe9b008a7a7b4e71de39192ceec60825d9f47
Giovanni Martina <drayah@gmail.com>
2018-11-24 13:36:09 -0200

    Change function name

--- src/wallchart/expenses/utils/io.clj
+++ src/wallchart/expenses/utils/io.clj
@@ -2,7 +2,7 @@
   (:require [clojure.java.io :as io]
             [clojure.data.csv :as csv]))
 
-(defn read-expenses-csv
+(defn read-expenses-csv!
   "Read lines from a csv and return a seq"
   [csv-path]
   (with-open [reader (io/reader csv-path)]
